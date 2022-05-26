69895baaaa1159e9f55af9421aa35bee99795770
Nakahara Akihiro@10yro <nakahara.akihiro@10yro.co.jp>
2022-05-26 14:30:22 +0900

    ログを調整

--- app/Services/InfoPageAccessService.php
+++ app/Services/InfoPageAccessService.php
@@ -18,6 +18,8 @@ class InfoPageAccessService
     public function save($param)
     {
         try {
+            error_log('#start InfoPageAccessService#save');
+
             DB::beginTransaction();
 
             $infoPageAccess = InfoPageAccess::create([
@@ -28,9 +30,12 @@ class InfoPageAccessService
 
             DB::commit();
 
+            error_log('#end InfoPageAccessService#save');
+
             return true;
         } catch (\Throwable $e) {
             DB::rollBack();
+            error_log($e);
             return false;
         }
     }
